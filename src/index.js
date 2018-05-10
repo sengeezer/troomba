07a37b5003a3b8d5de51e7d7fa0b23fc549bc18a
Fabio Albertin <albertin@gmail.com>
2018-05-11 00:32:53 +0100

    Initial sketch

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = path.join(__dirname, 'input.txt');
+
+const directions = {
+  N: [1, 0],
+  E: [0, 1],
+  S: [-1, 0],
+  W: [-1, 0],
+};
+
+fs.readFile(input, 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const allData = data.split('\n');
+  console.log(allData);
+
+  const dataSetLength = allData.length;
+  const gridSize = allData[0];
+  const allMoves = allData[dataSetLength - 1];
+  const allStains = allData.slice(allData[1], allData[dataSetLength - 2]);
+});
