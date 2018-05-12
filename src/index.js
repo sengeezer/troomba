3d8f8b75ee3a6d07657f34341c90873d12504b4e
Fabio Albertin <albertin@gmail.com>
2018-05-13 00:55:37 +0100

    Progress towards processing moves

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const directions = {
   W: [-1, 0],
 };
 
+const origin = [0, 0];
+
+function process(arr1, arr2) {
+  return arr1.map(((num, idx) => num + arr2[idx]));
+}
+
 fs.readFile(input, 'utf8', (err, data) => {
   if (err) {
     throw err;
@@ -20,7 +26,9 @@ fs.readFile(input, 'utf8', (err, data) => {
   const dataSetLength = allData.length;
   const gridSize = allData[0];
   const allMoves = [...allData[dataSetLength - 1]];
-  const allStains = allData.slice(allData[1], allData[dataSetLength - 2]);
+  const allStains = allData.slice(1, dataSetLength - 2);
 
-  console.log(allMoves);
+  console.log(`Grid size: ${gridSize}\nMoves: ${allMoves}\nStains: ${allStains}`);
+
+  // console.log(`Debug: ${allData[dataSetLength - 2]}`);
 });
