19f8ea522659fe59ab79887a4072c287d74d38e1
Fabio Albertin <albertin@gmail.com>
2018-05-14 00:15:59 +0100

    Improved data formatting

--- src/index.js
+++ src/index.js
@@ -12,8 +12,21 @@ const directions = {
 
 const origin = [0, 0];
 
-function process(arr1, arr2) {
-  return arr1.map(((num, idx) => num + arr2[idx]));
+function step(arr1, arr2) {
+  return arr1.map((num, idx) => num + arr2[idx]);
+}
+
+function checkSkid(arr, max, min) {
+  return arr.map((num) => {
+    switch (num) {
+      case num > max:
+        return max;
+      case num < min:
+        return min;
+      default:
+        return num;
+    }
+  });
 }
 
 fs.readFile(input, 'utf8', (err, data) => {
@@ -24,11 +37,12 @@ fs.readFile(input, 'utf8', (err, data) => {
   console.log(allData);
 
   const dataSetLength = allData.length;
-  const gridSize = allData[0];
+  const gridSize = [...allData[0]].filter(entry => entry !== ' ').map(num => Number(num));
   const allMoves = [...allData[dataSetLength - 1]];
-  const allStains = allData.slice(1, dataSetLength - 2);
+  const allStains = allData.slice(1, dataSetLength - 1).map(coord => [...coord].filter(entry => entry !== ' ').map(num => Number(num)));
 
   console.log(`Grid size: ${gridSize}\nMoves: ${allMoves}\nStains: ${allStains}`);
 
-  // console.log(`Debug: ${allData[dataSetLength - 2]}`);
+  // Type debugging
+  console.log(gridSize);
 });
