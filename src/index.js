8ba64bb42b283837917d077069d6154abbeb2660
Fabio Albertin <albertin@gmail.com>
2018-05-14 23:52:43 +0100

    Teaching Roomba to run

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const directions = {
 };
 
 const origin = [0, 0];
+let cursor = [];
 
 function step(arr1, arr2) {
   return arr1.map((num, idx) => num + arr2[idx]);
@@ -38,15 +39,25 @@ fs.readFile(input, 'utf8', (err, data) => {
     throw err;
   }
   const allData = data.split('\n');
-  console.log(allData);
 
   const dataSetLength = allData.length;
   const gridSize = [...allData[0]].filter(entry => entry !== ' ').map(num => Number(num));
   const allMoves = [...allData[dataSetLength - 1]];
   const allStains = allData.slice(1, dataSetLength - 1).map(coord => [...coord].filter(entry => entry !== ' ').map(num => Number(num)));
 
-  console.log(`Grid size: ${gridSize}\nMoves: ${allMoves}\nStains: ${allStains}`);
 
-  // Type debugging
-  console.log(compareValues([1, 1], [1, 1]));
+  // debugging
+  console.log(allData);
+  console.log(`Grid size: ${gridSize}\nMoves: ${allMoves}\nStains: ${allStains}`);
+  console.log(directions[allMoves[0]]);
+
+  // Make one move
+  cursor = step(origin, directions[allMoves[0]]);
+  console.log(cursor);
+
+  // check for stain
+
+  // Check skid
+  cursor = checkSkid(cursor, gridSize[0], gridSize[1]);
+  console.log(cursor);
 });
